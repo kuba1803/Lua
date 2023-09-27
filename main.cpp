50f8586b39b3fb702911959c707cbee4920ae7ef
Jakub <kuba1803@ymail.com>
2023-09-27 22:47:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::ios::sync_with_stdio(false);
+    if( argc > 1){
+
+    } else if( argc = 1) {
+
+    } else {
+        
+    }
+    return 0;
+}
